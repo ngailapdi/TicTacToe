2673f1834c2c0b7f5e6ac3921c0554254380db80
Ngài Lập Dị <ngailapdi@gmail.com>
2017-02-24 22:18:13 -0500

    AI complete

---
+++ AI.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class AI {
+	public int minimax(Board currentBoard, int depth, int count) {
+		int score = currentBoard.evaluateAI();
+		if (score != 0) return score;
+		if (!currentBoard.isNotFull()) return 0;
+		if (count % 2 == 1) {
+			int max = Integer.MIN_VALUE;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (currentBoard.board[i][j] == 1 || currentBoard.board[i][j] == 2) {
+						continue;
+					}
+					currentBoard.board[i][j] = 1;
+					max = Math.max(max, minimax(currentBoard, depth + 1, count + 1));
+					currentBoard.board[i][j] = 0;
+				}
+			}
+			return max;
+		} else {
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (currentBoard.board[i][j] == 1 || currentBoard.board[i][j] == 2) {
+						continue;
+					}
+					currentBoard.board[i][j] = 2;
+					min = Math.min(min, minimax(currentBoard, depth + 1, count + 1));
+					currentBoard.board[i][j] = 0;
+				}
+			}
+			return min;
+		}
+	}
+	public int[] generateNextMove(Board currentBoard) {
+		int minVal = Integer.MAX_VALUE;
+		int[] res = new int[2];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (currentBoard.board[i][j] != 0) continue;
+				currentBoard.board[i][j] = 2;
+				int move = minimax(currentBoard, 0, 1);
+				currentBoard.board[i][j] = 0;
+				if (move < minVal) {
+					minVal = move;
+					res[0] = i;
+					res[1] = j;
+				}
+			}
+		}
+		return res;
+	}
+}
